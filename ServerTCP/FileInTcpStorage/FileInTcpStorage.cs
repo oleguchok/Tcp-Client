4cc84874f79d4407df1b08d6dfee00e9eade1c4c
oleguhcok <oleguchok@gmail.com>
2015-05-07 22:50:39 +0300

    Adding File to transfer model and client with server

---
+++ ServerTCP/FileInTcpStorage/FileInTcpStorage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileInTcpStorage
+{
+    public class FileInTcpStorage
+    {
+        public string Name { get; set; }
+        public int Offset { get; set; }
+        public int SizeOfBlock { get; set; }
+    }
+}
